4fa98286e7f0a6a93c0a6c81295ee6b0db5c24e2
Arbaaz Meghani <amegha3@uic.edu>
2017-10-24 21:33:13 -0500

    Started working on Unity's Netowrking tutorial.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+	public override void OnStartLocalPlayer() {
+		GetComponent().material.color = Color.blue;
+	}
+
+	void Update() {
+		if (!isLocalPlayer)
+			return;
+		float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		float verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, horizontalMovement, 0);
+		transform.Translate(0, 0, verticalMovement);
+	}
+}
