9282ecdb4ccf09c16c81373e62393320eea43362
Arbaaz Meghani <amegha3@uic.edu>
2017-11-18 10:08:48 -0600

    Added networked health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour {
+public class Health : NetworkBehaviour {
 
 	public const int maxHealth = 100;
 	public RectTransform healthBar;
 
+	[SyncVar(hook = "OnChangeHealth")]
 	private int currentHealth = maxHealth;
 
 	public void TakeDamage(int amount) {
+		if (!isServer)
+			return;
 		currentHealth -= amount;
 		if (currentHealth <= 0) {
 			currentHealth = 0;
@@ -17,5 +21,9 @@ public class Health : MonoBehaviour {
 		}
 
 		healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+	}
+
+	void OnChangeHealth(int health) {
+		healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
 	}
 }
