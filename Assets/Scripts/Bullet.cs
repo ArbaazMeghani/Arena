94d5597fbef28020ff45dca732eeaf43a09b631c
Arbaaz Meghani <amegha3@uic.edu>
2017-11-17 21:18:33 -0600

    Added health to the player and ability to take damage when hit with bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
+	public int damageAmount = 10;
+
 	void OnCollisionEnter(Collision other) {
 		Destroy (gameObject);
+		if(other.gameObject.CompareTag("Player")) {
+			gameObject.GetComponent<Health> ().TakeDamage (damageAmount);
+		}
 	}
 }
